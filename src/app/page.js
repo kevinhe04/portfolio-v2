08eee8b2fd9784edbbea3216fc2031b94bc27f74
Kevin He <k.he@sunlife.com>
2025-06-29 15:46:14 -0400

    layout change

--- src/app/page.js
+++ src/app/page.js
@@ -30,7 +30,7 @@ export default function Home() {
               transition={{ duration: 0.8 }}
               className="text-center lg:text-left"
             >
-              <div className="md:mb-8 mb-20">
+              <div className="mb-8 mt-20 md:mt-0">
                 <img
                   src="images/headshot.jpeg"
                   alt="Kevin He"
