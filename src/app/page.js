dc8e100eba663ee4765b230e80de5ba1f7ef78e6
Kevin He <k.he@sunlife.com>
2025-07-02 18:54:16 -0400

    no footer

--- src/app/page.js
+++ src/app/page.js
@@ -499,9 +499,6 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <footer className="py-6 bg-gray-800 text-white text-center">
-        <p>&copy; 2025 Kevin He. All rights reserved.</p>
-      </footer>
     </div >
   );
 }
